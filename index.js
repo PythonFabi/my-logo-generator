9aae8101195ac61d6367a29d36deef20e2548b18
PythonFabi <129299589+PythonFabi@users.noreply.github.com>
2023-06-28 22:54:19 -0400

    write comments for the functionality of index.js, shapes.js, and shapes.test.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// import inquirer, fs and the three classes: triangle, circle and square
 const inquirer = require('inquirer');
 const fs = require('fs');
 const {Triangle, Circle, Square} = require('./lib/shapes');
 
+// questions for the prompt and user input to choose the logo spcifications
 const questions = [
     {
         type: 'input',
@@ -15,6 +17,7 @@ const questions = [
         type: 'list',
         message: 'Which of the following three shapes(circle, triangle, and square) should your logo have?',
         name: 'shape',
+        // let user choose between those three shapes
         choices: ['circle', 'triangle', 'square']
     }, {
         type: 'input',
@@ -23,11 +26,14 @@ const questions = [
     }
 ];
 
-
+// function to write the svg file, with all the user input stored in variables
 function writeToFile(fileName, { text, tcolor, shape, scolor}) {
+    // variable to create shape based on user input
     let createShape;
+    // switch cases which will choose the classes based on the user input
     switch (shape) {
         case 'circle':
+            // inside of the class is scolor as a passed argument, since the color of the shape can be chosen too
             createShape = new Circle(scolor);
             break;
         case 'triangle':
@@ -37,10 +43,11 @@ function writeToFile(fileName, { text, tcolor, shape, scolor}) {
             createShape = new Square(scolor);
             break;
             default:
+                // error case
                 throw new Error('Invalid shape choice'); 
     }
 
-
+    // template for the svg file passing in the shape classes and render function performed, based on the class and the rest of the variables passed in the template
     const svgTemplate = `<svg version="1.1" 
     width="300" height="200" 
 
@@ -52,10 +59,12 @@ function writeToFile(fileName, { text, tcolor, shape, scolor}) {
 
     </svg>`;
 
+    // fs write file, takes filename and the svgTemplate with all the inserted template literals
     fs.writeFileSync(fileName, svgTemplate);
 
 }
 
+// initialize inquirer, with the questions, which takes the answers to create logo.svg inside the examples folder and passes the answers as arguments in the writeToFile function
 inquirer.prompt(questions).then((answers) => {
     writeToFile('./examples/logo.svg', answers);
 }) 
